5e3b8dc1bf558b6a414051d4e16b741aa92f6ec3
Lingfey <bokanchalangman@gmail.com>
2022-10-07 03:16:54 +0800

    Address
    Introduction to Classes

---
+++ Homework_7_1_Address/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Homework_7_1_Address
+{
+    public class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Bldg { get; set; }
+        public string Apt { get; set; }
+        public string Zip { get; set; }
+
+        public void getAddress()
+        {
+            Console.WriteLine($"Country: {Country}\nCity: {City}\n" +
+                $"Address: {Bldg}, {Street}\nApartment No: {Apt}\nZip code: {Zip} ");
+        }
+    }
+}
+
---
+++ Homework_7_1_Address/Program.cs
@@ -0,0 +1,27 @@
+﻿////Используя Visual Studio, создайте проект по шаблону Console Application.
+//Создайте программу в которой создайте класс «Адрес». 
+//В теле класса создайте следующие поля: страна, город, улица, дом, квартира, индекс.
+//Для каждого поля, создать свойство с двумя методами доступа. Также в классе
+//необходимо создать метод для отображения информации про адрес.
+//В методе Main() создайте экземпляр класса «Адрес», присвойте всем полям значение через свойства доступа,
+//а также на экземпляр класса вызовите метод, который выводит информацию про адрес.
+//using System;
+
+namespace Homework_7_1_Address
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address userAddress = new Address();
+            userAddress.Country = "Ukraine";
+            userAddress.City = "Kyiv";
+            userAddress.Street = "Peremohy prosp.";
+            userAddress.Bldg = "8b";
+            userAddress.Apt = "1098";
+            userAddress.Zip = "08932";
+
+            userAddress.getAddress();
+        }
+    }
+}
